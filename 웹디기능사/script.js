48956fe94e9074da80e2b3b08335758adac73890
사재원 <aion97@naver.com>
2024-09-27 21:26:28 +0900

    웹디자인기능사

--- 웹디기능사/script.js
+++ 웹디기능사/script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function(){
     $(`.navi>li`).mouseover(function(){
         $(`.submenu`).stop().slideDown(500);
         $(`#menu_bg`).stop().slideDown(500);
-    }).mouseover(function(){
+    }).mouseout(function(){
         $(`.submenu`).stop().slideUp(500);
         $(`#menu_bg`).stop().slideUp(500);
     });
