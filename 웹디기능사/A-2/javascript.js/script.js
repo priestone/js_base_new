19721e6680de38ca163ed44e84b820fa8f224d87
사재원 <aion97@naver.com>
2024-09-28 12:25:09 +0900

    웹디 연습 A-2
    메뉴 자바스크립트 구현까지만 가능

--- 웹디기능사/A-2/javascript.js/script.js
+++ 웹디기능사/A-2/javascript.js/script.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function(){
 
-    $(`.navi>li`).mouseover(function(){
-        $(`.submenu`).stop().slideDown(500);
+    $(`.menu`).mouseover(function(){
+        $(`.sub_menu_wrap`).stop().slideDown(500);
         $(`#menu_bg`).stop().slideDown(500);
     }).mouseout(function(){
-        $(`.submenu`).stop().slideUp(500);
+        $(`.sub_menu_wrap`).stop().slideUp(500);
         $(`#menu_bg`).stop().slideUp(500);
     });
 
